3e7a0bdea4fe9542eebdc59b9575ce0a342bcb40
Prograpper <bbh1988@naver.com>
2016-02-23 21:38:49 +0900

    MySQL - department-highest-salary.sql

---
+++ leetcode/department-highest-salary.sql
@@ -0,0 +1,32 @@
+/*
+- URL : https://leetcode.com/problems/department-highest-salary/
+- TYPE : MySQL
+
+The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+
+The Department table holds all departments of the company.
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+
+Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, Max has the highest salary in the IT department and Henry has the highest salary in the Sales department.
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| Sales      | Henry    | 80000  |
++------------+----------+--------+
+*/
+
+SELECT d.Name AS Department, e.Name AS Employee, h.Salary AS Salary FROM Employee e, Department d, (SELECT max(Salary) AS Salary, DepartmentId FROM Employee GROUP BY DepartmentId) h WHERE e.Salary = h.Salary AND e.DepartmentId = h.DepartmentId AND e.DepartmentId = d.Id;
